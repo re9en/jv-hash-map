80ec045bdc39631f9eeae4c019a5bbf496bfcd8e
Timofii Kniahnytskiy <regen69neger@gmail.com>
2026-03-01 13:24:07 +0200

    made my hashMap
    overrided put()
    getSize
    getValue

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,97 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private int size;
+    private Node[] table = new Node[DEFAULT_CAPACITY];
+    private int capacity = DEFAULT_CAPACITY;
+    private final double loadFactor = 0.75;
 
     @Override
     public void put(K key, V value) {
+        int hash = (key == null) ? 0 : key.hashCode();
+        Node<K, V> node = new Node<>(hash, key, value, null);
 
+        if (size >= capacity * loadFactor) {
+            resize();
+        }
+
+        int index = (hash & 0x7fffffff) % capacity;
+
+        if (table[index] == null) {
+            table[index] = node;
+            size++;
+            return;
+        }
+
+        Node<K, V> curr = table[index];
+
+        while (curr != null) {
+            if (key == null ? curr.key == null : key.equals(curr.key)) {
+                curr.value = value;
+                return;
+            }
+
+            if (curr.next == null) {
+                curr.next = node;
+                size++;
+                return;
+            }
+            curr = curr.next;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int hash = (key == null) ? 0 : key.hashCode();
+        int index = (hash & 0x7fffffff) % capacity;
+        Node<K, V> curr = table[index];
+        while (curr != null) {
+            if (key == null ? curr.key == null : key.equals(curr.key)) {
+                return curr.value;
+            }
+            curr = curr.next;
+        }
+
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        final Node<K, V>[] oldTable = table;
+        capacity = capacity << 1;
+
+        Node<K, V>[] newTable = new Node[capacity];
+        table = newTable;
+        size = 0;
+
+        for (Node<K, V> record : oldTable) {
+            if (record != null) {
+                Node<K, V> curr = record;
+                while (curr != null) {
+                    put(curr.key, curr.value);
+                    curr = curr.next;
+                }
+            }
+        }
+    }
+
+    public static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
+
